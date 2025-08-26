a612f0d37bbe6327c4c4c7a530fa8ff0832bbfab
Kavya <kavya835282@gmail.com>
2025-08-25 22:28:29 -0400

    add functionality with playwright

--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+from playwright.sync_api import sync_playwright
 import smtplib                      #sending emails
 import pandas as pd                 #filtering, reading and writing data
 import requests                     #sending HTTP requests
@@ -26,22 +27,39 @@ def process_products(df):
     for product in df.to_dict("records"):   #converts DataFrame into a list of dictionaries
         # product is a dictionary which uses url as the key to identify the URL
         # i.e. product["url"] is the URL
-        html = get_response(product["url"])
+
+        with sync_playwright() as p:
+            browser = p.chromium.launch(headless=True)  # set headless=False to watch it run
+            page = browser.new_page()
+            page.goto(product["url"], timeout=60000)
+
+            # Wait until the price element is present in the DOM
+            page.wait_for_selector(".product-sales-price")
+
+            # Get the fully rendered HTML
+            html = page.content()
+
+            browser.close()
+
         product["price"] = get_price(html)  #new key-value of price -> price-value
-        product["alert"] = product["price"] < product["alert_price"]    #boolean value
+        if(product["price"] > 0):
+            product["alert"] = product["price"] < product["alert_price"]    #boolean value
+        else:
+            product["alert"] = False
+
         updated_products.append(product)    #product price is done being checked
         #continue this for all the products
     return pd.DataFrame(updated_products)   #new DataFrame
 
-#get the HTML
-def get_response(url):
-    response = requests.get(url)
-    return response.text
-
 def get_price(html):
+    price = 0
     soup = BeautifulSoup(html, "lxml")  #creating a BeautifulSoup object using the HTML response
     el = soup.select_one(".product-sales-price")    #'price_color' is the class of the HTML element #the . in front of it is used to select the elements of this class in the CSS
-    price = Price.fromstring(el.text)   #el.text contains the price and currency symbol which needs to be removed
+    if el:
+        price = Price.fromstring(el.get_text(strip=True))
+        price = Price.fromstring(el.text)   #el.text contains the price and currency symbol which needs to be removed
+    else:
+        print("Price element not found")
     return price.amount_float
 
 
